dcef19c28d82e6cf301c4d2956b616467ed13858
nguyenvanbinh05 <ugnhnib@gmail.com>
2026-06-12 10:48:58 +0700

    fist commit

---
+++ tests/test_basic.py
@@ -0,0 +1,183 @@
+"""
+    Basic tests for the PupDB database.
+"""
+
+import logging
+import os
+import json
+
+import pytest
+
+from pupdb.core import PupDB
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s | %(process)d | %(levelname)s | %(message)s'
+)
+
+
+TEST_DB_PATH = 'testdb.json'
+TEST_DB_LOCK_PATH = '{}.lock'.format(TEST_DB_PATH)
+
+
+@pytest.fixture(autouse=True)
+def run_around_tests():
+    """ Function is invoked around each test run. """
+
+    logging.debug('Test started.')
+    yield
+    logging.debug('Test ended.')
+
+    if os.path.exists(TEST_DB_PATH):
+        os.remove(TEST_DB_PATH)
+
+    if os.path.exists(TEST_DB_LOCK_PATH):
+        os.remove(TEST_DB_LOCK_PATH)
+
+
+def test_get_and_set():
+    """ Tests the get() and set() methods of PupDB. """
+
+    database = PupDB(TEST_DB_PATH)
+    key, val = 'test_key', list(range(100))
+    database.set(key, val)
+
+    assert database.get(key) == val
+
+
+def test_remove():
+    """ Tests the remove() method of PupDB. """
+
+    database = PupDB(TEST_DB_PATH)
+    for i in range(10):
+        database.set(i, i)
+
+    # Removing key 0 from the database.
+    database.remove(0)
+
+    for i in range(10):
+        if i == 0:
+            assert database.get(i) is None
+        else:
+            assert database.get(i) is not None
+
+    # Check the behavaiour for a non-existent key.
+    with pytest.raises(KeyError):
+        database.remove(1000)
+
+
+def test_keys():
+    """ Tests the keys() method of PupDB. """
+
+    database = PupDB(TEST_DB_PATH)
+    range_list = [str(i) for i in range(10)]
+    for i in range_list:
+        database.set(i, i)
+
+    db_keys_list = list(database.keys())
+
+    assert sorted(db_keys_list) == range_list
+
+
+def test_values():
+    """ Tests the values() method of PupDB. """
+
+    database = PupDB(TEST_DB_PATH)
+    range_list = list(range(10))
+    for i in range_list:
+        database.set(i, i)
+
+    db_values_list = list(database.values())
+
+    assert sorted(db_values_list) == range_list
+
+
+def test_items():
+    """ Tests the items() method of PupDB. """
+
+    database = PupDB(TEST_DB_PATH)
+    range_list = list(range(10))
+    for i in range_list:
+        database.set(i, i)
+
+    items_list = [(str(i), i) for i in range_list]
+    db_items_list = list(database.items())
+
+    assert sorted(db_items_list) == items_list
+
+
+def test_dumps():
+    """ Tests the dumps() method of PupDB. """
+
+    database = PupDB(TEST_DB_PATH)
+    range_list = list(range(10))
+    for i in range_list:
+        database.set(i, i)
+
+    db_dict = {str(i): i for i in range_list}
+
+    assert database.dumps() == json.dumps(db_dict, sort_keys=True)
+
+
+def test_truncate_db():
+    """ Tests the truncate_db() method of PupDB. """
+
+    database = PupDB(TEST_DB_PATH)
+    range_list = list(range(10))
+    for i in range_list:
+        database.set(i, i)
+
+    db_dict = {str(i): i for i in range_list}
+    assert database.dumps() == json.dumps(db_dict, sort_keys=True)
+
+    database.truncate_db()
+
+    assert database.dumps() == json.dumps({})
+
+
+def test_length():
+    """
+        Tests whether len() on the PupDB instance returns it's proper length. 
+    """
+
+    database = PupDB(TEST_DB_PATH)
+    num_items = 100
+    range_list = list(range(num_items))
+    for i in range_list:
+        database.set(i, i)
+    assert len(database) == 100
+
+
+def test_get_database():
+    """
+        Tests whether _get_database() returns the dict for the DB.
+    """
+
+    database = PupDB(TEST_DB_PATH)
+    num_items = 100
+    range_list = list(range(num_items))
+    for i in range_list:
+        database.set(i, i)
+
+    # pylint: disable=protected-access
+    db_dict = database._get_database()
+    assert json.dumps({str(i): i for i in range_list}, sort_keys=True) == \
+        json.dumps(db_dict, sort_keys=True)
+
+
+def test_flush_database():
+    """
+        Tests whether _flush_database() writes to DB.
+    """
+
+    database = PupDB(TEST_DB_PATH)
+    num_items = 100
+    range_list = list(range(num_items))
+    data_dict = {str(i): i for i in range_list}
+
+    # pylint: disable=protected-access
+    database._flush_database(data_dict)
+    db_dict = database._get_database()
+
+    assert json.dumps(data_dict, sort_keys=True) == \
+        json.dumps(db_dict, sort_keys=True)
---
+++ tests/test_multithreaded.py
@@ -0,0 +1,133 @@
+"""
+    Tests to perform reads/writes to the database concurrently i.e.
+    from multiple threads.
+"""
+
+import logging
+import os
+from threading import Thread
+import json
+import sys
+
+import pytest
+
+from pupdb.core import PupDB
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s | %(process)d | %(levelname)s | %(message)s'
+)
+
+
+TEST_DB_PATH = 'testdb.json'
+TEST_DB_LOCK_PATH = '{}.lock'.format(TEST_DB_PATH)
+
+
+class PupDBWriterThread(Thread):
+    """
+        This class represents an instance of a thread that writes
+        to the database.
+    """
+
+    def __init__(self, data_range, database=None):
+        """ Instance Initialization """
+
+        super(PupDBWriterThread, self).__init__()
+        self.data_range = data_range
+        if database is None:
+            self.database = PupDB(TEST_DB_PATH)
+        else:
+            self.database = database
+
+    def run(self):
+        """ Method performs write to the database."""
+
+        for i in self.data_range:
+            self.database.set(i, i)
+
+
+@pytest.fixture(autouse=True)
+def run_around_tests():
+    """ Function is invoked around each test run. """
+
+    logging.debug('Test started.')
+    yield
+    logging.debug('Test ended.')
+
+    if os.path.exists(TEST_DB_PATH):
+        os.remove(TEST_DB_PATH)
+
+    if os.path.exists(TEST_DB_LOCK_PATH):
+        os.remove(TEST_DB_LOCK_PATH)
+
+
+def test_mt_get_and_set_mi():
+    """
+        Tests the get() and set() methods of PupDB,
+        in a multi-threaded scenario, where each thread has it's own
+        instance (object) of PupDB.
+    """
+
+    data_ranges = [
+        range(1, 50),
+        range(50, 100),
+        range(100, 150),
+        range(150, 201)
+    ]
+    writers = []
+
+    # Write from multiple threads.
+    for data_range in data_ranges:
+        writer = PupDBWriterThread(data_range)
+        writers.append(writer)
+        writer.start()
+
+    for writer in writers:
+        writer.join()
+
+    # Verify if all keys have been written properly.
+    database = PupDB(TEST_DB_PATH)
+    assert len(database) == 200
+
+
+def test_mt_get_and_set_si():
+    """
+        Tests the get() and set() methods of PupDB,
+        in a multi-threaded scenario, where all threads share a
+        single instance (object) of PupDB.
+
+        PupDB currently does not support multiple threads using the same
+        PupDB instance, so the database file will get corrupted due to
+        race condition.
+
+        If you want to use PupDB with multiple threads, maintain separate
+        PupDB instance for each thread.
+    """
+
+    v_major, v_minor, _, _, _ = sys.version_info
+    if v_major < 3 or (v_major == 3 and v_minor <= 4):
+        error_cls = ValueError
+    else:
+        error_cls = json.decoder.JSONDecodeError
+
+    with pytest.raises(error_cls):
+        data_ranges = [
+            range(1, 50),
+            range(50, 100),
+            range(100, 150),
+            range(150, 201)
+        ]
+        writers = []
+        database = PupDB(TEST_DB_PATH)
+
+        # Write from multiple threads.
+        for data_range in data_ranges:
+            writer = PupDBWriterThread(data_range, database)
+            writers.append(writer)
+            writer.start()
+
+        for writer in writers:
+            writer.join()
+
+        # Verify if all keys have been written properly.
+        assert len(database) == 200
